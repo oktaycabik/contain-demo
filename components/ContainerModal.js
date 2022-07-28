7dbc32bb92e48183158ef0b8577efda516c4e42c
Contain Haus <mybook@Oktay.local>
2022-07-29 00:29:32 +0300

    responsive duzenleme

--- components/ContainerModal.js
+++ components/ContainerModal.js
@@ -5,7 +5,7 @@ const ContainerModal = ({data}) => {
   return (
     <>
  <div className='banner-img vh-100'  style={{ backgroundImage: `url(${data.image})` }}>
-    <div className="banner-content col-lg-4 col-md-6 col-sm-12">
+    <div className="banner-content col-lg-4 col-md-4 col-sm-6 col-12">
     <h5>{data.title.first}</h5>
     <h5>{data.title.second}</h5>
     <p>{data.content.first}</p>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Head from "next/head";
+
 import Script from "next/script";
 import Layout from "../components/Layout";
 import "../styles/style.css"
 import "../styles/globals.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    import("bootstrap/dist/js/bootstrap");
+},[])
   return (
     <>
    
