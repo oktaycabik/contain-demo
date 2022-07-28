2bf7e61930b4a6cf993c36bb2dfaee209c0f23f1
Contain Haus <mybook@Oktay.local>
2022-07-29 00:34:53 +0300

    duzenleme

--- components/ContainerInto.js
+++ components/ContainerInto.js
@@ -7,7 +7,7 @@ const ContainerInto = ({data}) => {
     <div className='intro-img'  style={{ backgroundImage: `url(${data.image})` }}>
     <div className="intro-content">
     <h1 className="mt-1 fw-bold">{data.title.second}</h1>
-    <h5 className="mt-1 fw-bolder text-danger">{data.content.second}</h5>
+    <h5 className="mt-1  text-danger">Direkt ab Werk</h5>
     <h5 className="mt-2 text-light">{data.content.first}</h5>
     <button className="intro-button">HIER GEHT'S WEITTER</button>
     </div>
