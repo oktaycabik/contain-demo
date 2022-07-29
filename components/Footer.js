9d82dc16de80cc6fe7e12aeab14738dadc98a289
Contain Haus <mybook@192.168.1.115>
2022-07-29 09:40:10 +0300

    duzenleme

--- components/Footer.js
+++ components/Footer.js
@@ -7,39 +7,63 @@ const Footer = () => {
       <footer className="footer">
         <div className="row ">
           <div className="col-lg-2 col-md-2 col-12">
-            <h5 className="mt-2">ÜBERSICHT</h5>
-            <div className="mt-2">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
+            <h5 className="mt-2 ">ÜBERSICHT</h5>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">FlatPack</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Wohncontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Bürocontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">XL Wohncontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Sanitärcontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Pförtnercontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Specialcontainer</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Cafe-Container</div>
+            </Link>
+            <Link href="/flat-pack">
+              <div className="mt-2 footer-color">Schulcontainer</div>
+            </Link>
+          </div>
+          <div className="col-lg-2 col-md-2 col-12">
+            {" "}
+            <h5 className="mt-2">KONTAKTIEREN SIE UNS</h5>
+            <div className="mt-2 footer-color">
+              Am Huchtert 44, 58456 Witten Deutschland
+            </div>
+            <div className="mt-3 footer-color">+49 2302 889 4880</div>
+            <div className="mt-3 footer-color">+49 173 622 1062</div>
+            <div className="mt-3 footer-color">info@containhaus.de</div>
           </div>
           <div className="col-lg-2 col-md-2 col-12">
             {" "}
             <h5 className="mt-2">ÜBERSICHT</h5>
-            <div className="mt-2">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-          </div>
-          <div className="col-lg-2 col-md-2 col-12">
-            {" "}
-            <h5 className="mt-2">ÜBERSICHT</h5>
-            <div className="mt-2">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
-            <div className="mt-3">Startseite</div>
+            <Link href="/impressum">
+              <div className="mt-2">IMPRESSUM</div>
+            </Link>
+            <Link href="/uber-uns">
+              <a className="mt-3">ÜBER UNS</a>
+            </Link>
+            <Link href="faq">
+              <div className="mt-3">FAQ</div>
+            </Link>
+            <Link href="agb">
+              <div className="mt-3">AGB</div>
+            </Link>
+            <Link href="catolog">
+              <div className="mt-3">CATALOG HERUNTERLADEN</div>
+            </Link>
           </div>
 
           <div className="col-lg-6 col-md-6 col-12">
@@ -49,118 +73,92 @@ const Footer = () => {
                 src="https://www.containhaus.de/assets/img/RedBlack.webp"
               />
             </div>
-            <div>CONTAINER UND MOBILE RAUMSYSTEME</div>
-            <p>Die Conliving GmbH bietet zahlreiche Containerarten und Containeranlagen an. Von Wohn – und Bürocontainern bis zu Sanitär – und Baustellencontainern, bieten wir eine umfangreiche Auswahl an Containern an. Sie können Container kaufen oder auch mieten. Wählen Sie dafür einfach eins unserer Conliving Pakete aus.</p>
+            <div className="mb-2">
+            <div>Inhaber: Abraham Wach</div>
+       <div>BANKNAME: Volksbank Sprockhövel</div> 
+       <div>SWIFTCODE: GENODEM1SPO</div> 
+       <div>KONTOINHABER: CONTAIN HAUS CONTAINER</div> 
+       <div>IBAN: DE4145 2615 4700 1398 6200</div> 
+       <div>Steuernummer. : 348/5203/3253</div> 
+            </div>
+     
+            <Link href="https://www.facebook.com/ContainHaus">
+              <a className="" >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  class="bi bi-facebook"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                </svg>
+              </a>
+            </Link>
+            <Link href="https://twitter.com/ContainHaus">
+              <a className="ms-4 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  class="bi bi-twitter"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                </svg>
+              </a>
+            </Link>
+            <Link href="https://www.instagram.com/containhaus/">
+              <a className="ms-4 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  class="bi bi-instagram"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                </svg>
+              </a>
+            </Link>
+            <Link href="https://tr.linkedin.com/company/contain-haus?trk=public_profile_experience-item_profile-section-card_image-click">
+              <a className="ms-4 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  class="bi bi-linkedin"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                </svg>
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCDbpL6rsln1udSYWcTARyGg">
+              <a className="ms-4 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="36"
+                  fill="white"
+                  class="bi bi-youtube"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
+                </svg>
+              </a>
+            </Link>
+        
           </div>
+          <p className="mt-5 text-center">© 2022, Developed by Contain Haus</p>
         </div>
       </footer>
     </>
-    // <div>
-    //   <div className="medium-footer">
-    //     <div className="container ">
-    //         <div className="footer-top">
-    //             <img className="footer-logo"  src="https://www.containhaus.de/assets/img/RedBlack.webp"/>
-    //         </div>
-    //       <div className="row justify-content-between">
-    //         <div className="col-6 aling-items-center">
-    //           <h5 className="contact-tag">KONTAKTIEREN SIE UNS</h5>
-
-    //           <div>Am Huchtert 44, 58456 Witten Deutschland</div>
-
-    //           <div className="footer-info">
-    //             <div>
-    //               <div>+49 2302 889 4880</div>
-    //             </div>
-    //             <div>
-    //               <div>+49 173 622 1062</div>
-    //             </div>
-    //             <div>
-    //               <span>info@containhaus.de</span>
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <div className="col-lg-6 col-md-6 col-sm-6 aling-items-center">
-    //           <div className="footer-info">
-    //             <div>
-    //               <span className="footer-contact-title">Inhaber:</span>
-    //               <span className="footer-info-text">Abraham Wach</span>
-    //             </div>
-    //             <div>
-    //               <span className="footer-contact-title">BANKNAME:</span>
-    //               <span className="footer-info-text">
-    //                 Volksbank Sprockhövel
-    //               </span>
-    //             </div>
-    //             <div>
-    //               <span className="footer-contact-title">SWIFTCODE:</span>
-    //               <span className="footer-info-text">GENODEM1SPO</span>
-    //             </div>
-    //             <div>
-    //               <span className="footer-contact-title">KONTOINHABER:</span>
-    //               <span className="footer-info-text">
-    //                 CONTAIN HAUS CONTAINER
-    //               </span>
-    //             </div>
-    //             <div>
-    //               <span className="footer-contact-title">IBAN:</span>
-    //               <span className="footer-info-text">
-    //                 DE4145 2615 4700 1398 6200
-    //               </span>
-    //             </div>
-    //             <div>
-    //               <span className="footer-contact-title">Steuernummer:</span>
-    //               <span className="footer-info-text">348/5203/3253</span>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className="mid-footer-bot ">
-    //         <h5>IMPRESSUM</h5>
-    //         <h5>ÜBER UNS</h5>
-    //         <h5>FAQ</h5>
-    //         <h5>AGB</h5>
-    //         <h5>CATALOG HERUNTERLADEN</h5>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="b-example-divider" />
-    //   <div className="footer-bottom">
-
-    //   <div className="container">
-    //     <footer className="py-3 my-4">
-    //       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-    //         <li className="nav-item">
-    //           <a href="#" className="nav-link px-2 text-muted">
-    //             Home
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#" className="nav-link px-2 text-muted">
-    //             Features
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#" className="nav-link px-2 text-muted">
-    //             Pricing
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#" className="nav-link px-2 text-muted">
-    //             FAQs
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#" className="nav-link px-2 text-muted">
-    //             About
-    //           </a>
-    //         </li>
-    //       </ul>
-    //       <p className="text-center text-muted">© 2021 Company, Inc</p>
-    //     </footer>
-    //   </div>
-    //   </div>
-    // </div>
+    
   );
 };
 export default Footer;
---
+++ pages/uber-uns.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Link from "next/link";
+const Überuns = () => {
+  return (
+   
+      <>
+        <div className="container agb-content">
+          <div className="row mb-90 pb-30">
+            <div className="col-lg-12">
+              <div className="caption ">
+                <h2 className="words chars splitting text-dark d-flex justify-content-center fz-50">
+                  <Link href="#" className="d-flex">
+                  <h2>ÜBER UNS </h2>  
+                  </Link>
+                </h2>
+
+                <p className="text-dark fz-20">
+                  <span className="font-bold">
+                    ContainHaus ist eine Marke der Sicherheit und Schnelligkeit
+                    im Bereich Container und vorgefertigte Bautechnologien.
+                  </span>
+                  <br />
+                  <br />
+                  ContainHaus erwies sich als ein gut organisiertes Unternehmen,
+                  das in der Lage ist, verschiedene Arten von Projekten in
+                  verschiedenen Ländern zur gleichen Zeit durchzuführen.
+                  <br />
+                  <br />
+                  <span className="font-bold">
+                    ''Wir brechen die Standards''{" "}
+                  </span>
+                 In Zusammenarbeit mit unseren Ingenieuren, haben wir für
+                  den Deutschen und Europäischen Markt die Standards gebrochen
+                  und das Konzept mit erweiterbaren Container in Länge, breite
+                  und Höhe auf den Markt gebracht. Durch die von unseren
+                  Experten weiter entwickelte ''Nahtlose Gewinde Technik'' sind
+                  wir flexibler und stabiler als alle unsere Mitbewerber. Wir
+                  Produzieren Ihre Container und Stahlhäuser nach Ihren
+                  gewünschten Massen und Bauen diese für Sie vor Ort auf. Durch
+                  die ''Nahtlose Gewinde Technik'' sind unsere Container und
+                  Stahlhäuser jederzeit erweiterbar, auch nach 5 Jahren Gebrauch
+                  können wir Ihrem Wunsch entgegenkommen und Ihr Container oder
+                  Stahlhaus in 1-2 Werktagen weitere Räume hinzufügen .
+                  <br />
+                  <br />
+                  ContainHaus produziert seine Produkte in der Bundesrepublik
+                  Deutschland - Hessen und betreibt verschiedene Bauprojekte wie
+                  Baustellen, Notunterkünfte, Massenwohnungsprojekte,
+                  Feriendörfer, Kindertageseinrichtungen und vieles mehr.
+                  <br />
+                  <br />
+                  <span className="font-bold">Unsere Produkte:</span>
+                 Modulbau (Baustellen, Büros, Schulen und medizinische
+                  Einrichtungen, Militärlager) Wohncontainer
+                  (Schnellbaucontainer, Sanitärcontainer, Baustellen- und
+                  Bürocontainer) Modulare Kabinen Fertighäuser
+                  <br />
+                  <br />
+                  <span className="font-bold">
+                    Was machen wir anders:
+                  </span>
+                  <ol>
+                    <li>
+                      Wir Bauen unsere Container und Stahlhäuser vor Ort mit der
+                      einzigartigen Technik der ''Nahtlosen Gewinde System''
+                    </li>
+                    <li>Container Herstellung in allen maßen</li>
+                    <li> Moderne Container und Stahlhaus Bauten mit EnEV</li>
+                    <li> Herstellung nach den Deutschen Normen</li>
+                    <li>
+                      {" "}
+                      Projekte rechtzeitig liefern (schnelle Produktion und
+                      schnelle Montage)
+                    </li>
+                    <li>
+                      Stahlhäuser werden in 2 Wochen aufgebaut und
+                      schlüsselfertig übergeben.
+                    </li>
+                    <li>Kundendienst, die Ihre bedürfnisse versteht</li>
+                    <li>Hohe HSE-Maßstäbe</li>
+                    <li>Mitarbeiterwertsatz</li>
+                  </ol>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+   
+  );
+};
+
+export default Überuns;
