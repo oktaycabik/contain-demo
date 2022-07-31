9265923a98ab9fb894215cb6b25b0e616a54ec9a
Contain Haus <mybook@Oktay.local>
2022-07-31 19:24:36 +0300

    product single page

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 const ProductList = ({ product }) => {
   return (
     <>
       <div className="col-md-6 col-12 col-lg-3 mb-4 mb-lg-0 gx-3 mt-2">
         <div className="card text-black">
-          <Image width="100%" height="229px"  src={product.image.ima1} className="card-img-top" alt="iPhone" />
+          <Image
+            width="100%"
+            height="229px"
+            src={product.image.ima1}
+            className="card-img-top"
+            alt="iPhone"
+          />
           <div className="card-body card-info">
             <div className="text-center mt-1 ">
               <h4 className="card-title">{product.title}</h4>
-              <h5 className="text-dark mb-1 pb-3">
-                {product.content.text5}
-              </h5>
+              <h5 className="text-dark mb-1 pb-3">{product.content.text5}</h5>
               <h6 className="card-title mb-1 pb-3">{product.content.text2}</h6>
               <h6 className="card-title mb-1 pb-3">{product.content.text3}</h6>
               <h6 className="card-title mb-1 pb-3">{product.content.text6}</h6>
@@ -28,10 +32,13 @@ const ProductList = ({ product }) => {
                   Kontakt
                 </button>
               </Link>
-
-              <button type="button" className="btn btn-dark flex-fill ms-1">
-                Details
-              </button>
+ 
+                <button type="button" className="btn btn-dark flex-fill ms-1">
+                <Link href={`/container/${product.id}`}>
+                  Details
+                  </Link>
+                </button>
+           
             </div>
           </div>
         </div>
---
+++ pages/container/[id].js
@@ -0,0 +1,105 @@
+import React,{useState} from "react";
+import products from "../../data/ShopDeutsch.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+const Product = ({ product }) => {
+   
+  return (
+    <div>
+      <div className="container-fluid ProductPgBgColor">
+        <div className="container mt-10">
+          <div className="row mt-10">
+            <div className="col-lg-7 col-md-12 col-sm-12 mt-10">
+              <div>
+                <Swiper
+                  style={{
+                    "--swiper-navigation-color": "#fff",
+                    "--swiper-pagination-color": "#fff",
+                  }}
+                  loop={true}
+                  navigation={true}
+                  
+                  modules={[FreeMode, Navigation, Thumbs]}
+                  className="mySwiper2"
+                >
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima1} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima2} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima3} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima4} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima5} />
+                  </SwiperSlide>
+                </Swiper>
+                <Swiper
+                 
+                  loop={true}
+                  spaceBetween={5}
+                  slidesPerView={5}
+                  freeMode={true}
+                  watchSlidesProgress={true}
+                  modules={[FreeMode, Navigation, Thumbs]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima1} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima2} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima3} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima4} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img className="product-page-img" src={product.image.ima5} />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            </div>
+            <div className="col-lg-5 col-md-12 col-sm-12 p-5 bg-white border mt-10">
+              asdasd
+            </div>
+          </div>
+          <div className="mt-3 mb-3 pb-5 ">asdas</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
+
+export const getStaticPaths = async () => {
+  const paths = products.map((pro) => {
+    return {
+      params: { id: pro.id },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+};
+export const getStaticProps = async (context) => {
+  const product = products.find((pro) => pro.id === context.params.id);
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
