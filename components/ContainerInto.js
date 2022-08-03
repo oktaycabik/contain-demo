6c66e1f8f49afe5d1c45e2aa9c099e3b6a251bf8
Contain Haus <mybook@192.168.1.115>
2022-08-03 11:28:57 +0300

    text color

--- components/ContainerInto.js
+++ components/ContainerInto.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import windowSize from "../utils/windowSize";
-import Image from "next/image"
-const ContainerInto = ({ data }) => {
+
+const ContainerInto = ({ data,className }) => {
   const [width] = windowSize();
 
   const router = useRouter();
@@ -26,9 +26,9 @@ const ContainerInto = ({ data }) => {
   <>
      <div className="intro-img" style={{ backgroundImage: `url(${test()})` }}>
        <div className="intro-content">
-        <h1 className="mt-1 fw-bold">{data.title.second}</h1>
+        <h1 className={`mt-1 fw-bold ${router.asPath ==="/sanitar-container" || router.asPath === "/special-edition" ? 'text-light' : "text-dark"}`}>{data.title.second}</h1>
 
-        <h5 className="mt-1  text-danger">Direkt ab Werk</h5>
+        <h5 className={`mt-1  ${router.asPath ==="/sanitar-container" || router.asPath === "/special-edition" ? 'text-light' : "text-danger"}`}>Direkt ab Werk</h5>
          <h5 className="mt-2 text-light">{data.content.first}</h5>
         <Link href={`${data.href}`}>
            <button className="intro-button">HIER GEHT'S WEITTER</button>
